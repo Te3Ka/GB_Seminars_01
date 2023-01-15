cd81e82354ece97148f49ecf55d22583fe3d574a
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-15 13:09:30 +0300

    Задание 3

---
+++ Exercise03/Program.cs
@@ -0,0 +1,42 @@
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+/*
+Напишите программу, которая выводит название дня недели по введённому номеру
+3 - Среда
+5 - Пятница
+*/
+
+Console.WriteLine("Программа показывает название дня недели на русском языке по введённому номеру");
+Console.Write("Введите одно целое число от 1 до 7 = ");
+int numDay = Int32.Parse(Console.ReadLine());
+
+switch (numDay)
+{
+    case 1: 
+        Console.WriteLine("Понедельник");
+        break;
+    case 2: 
+        Console.WriteLine("Вторник");
+        break;
+    case 3: 
+        Console.WriteLine("Среда");
+        break;
+    case 4: 
+        Console.WriteLine("Четверг");
+        break;
+    case 5: 
+        Console.WriteLine("Пятница");
+        break;
+    case 6: 
+        Console.WriteLine("Суббота");
+        break;
+    case 7: 
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Такого дня не существует.");
+        break;
+}
