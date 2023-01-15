2c02cdf8297c13e161c9c12bc1a9cb0b510a20a5
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-15 12:27:16 +0300

    Задание 1

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+/*
+Напишите программу, которая на вход принимает на вход число и выводит квадрат этого числа.
+3 -> 9
+-5 -> 25
+-7 -> 49
+*/
+
+Console.WriteLine("Программа показывает максимальное и минимальное число из двух целых чисел");
+
+Console.WriteLine("Введите два целых числа через Enter");
+Console.Write("Число 1 = ");
+int num = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Квадрат введённого числа " + num + " является = " + num * num);
